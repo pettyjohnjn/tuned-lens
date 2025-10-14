f578f5d0702a5169b1c57870fc6811dd2814c676
Jordan <pettyjohnjn@polaris-login-01.hsn.cm.polaris.alcf.anl.gov>
2025-10-14 21:05:04 +0000

    Updated .gitignore

--- tuned_lens/scripts/trainer/enums.py
+++ tuned_lens/scripts/trainer/enums.py
@@ -4,7 +4,6 @@ class LossChoice(enum.Enum):
     """Options of what loss to select when training the model."""
     CE = "ce"
     KL = "kl"
-    SUBSET_KL = "subset_kl"
 
 class LensVariant(enum.Enum):
     TUNED = "tuned"
